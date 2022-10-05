f39567cf25da3c4ca6ef959607a6aa281b7cea15
Dawidx3p <maildawcio@gmail.com>
2022-10-05 08:13:16 +0200

    test

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,16 @@
-import SmallCard from '../components/SmallCard';
-import { projectIcons } from '../components/Icons';
+import React, { useState, useEffect } from 'react';
 
-import { projects } from '../utils/projectsData';
+function App() {
+  const [data, setData] = useState('');
 
-const Home = () => (
-  <div className="home">
-    <h1>What Can I Deploy to Static Apps?</h1>
-    <div className="card-grid">
-      {projects.map((project) => {
-        const Icon = projectIcons[project.id];
-        return (
-          <SmallCard
-            key={project.id}
-            Icon={Icon}
-            title={project.name}
-            slug={project.slug}
-          />
-        );
-      })}
-    </div>
-  </div>
-);
+  useEffect(() => {
+    (async function () {
+      const { text } = await( await fetch(`/api/message`)).json();
+      setData(text);
+    })();
+  });
 
-export default Home;
+  return <div>{data}</div>;
+}
+
+export default App;
